612eba23261707abe4182cc2bdab693b0d05e1cf
Snesnopic <snesnopic@gmail.com>
2026-06-13 10:27:48 +0200

    Fix Mode 6 and 7 boundary logic and dval2 bugs to perfectly match takc.exe

--- src/core/tak_encoder.cpp
+++ src/core/tak_encoder.cpp
@@ -225,6 +225,7 @@ EncodeResult Encoder::encode_stream(std::istream& is, std::ostream& os, const En
         }
 
         // Encode channels
+        std::cerr << "Before encode_channel: c[0][11022]=" << c[0][11022] << std::endl;
         for (int ch = 0; ch < channels; ch++) {
             encode_channel(c[ch].data(), current_frame_samples, bps, lpc_mode[ch], sample_rate, cfg, fw);
         }
@@ -233,7 +234,7 @@ EncodeResult Encoder::encode_stream(std::istream& is, std::ostream& os, const En
         if (channels == 2) {
             fw.write_bit(0); // nb_subframes - 1
             fw.write_bits(dmode_res.mode, 3);
-            if (dmode_res.mode >= 4) {
+            if (dmode_res.mode >= 4 && dmode_res.mode <= 5) {
                 if (dmode_res.shift > 0) {
                     fw.write_bit(1);
                     fw.write_bits(dmode_res.shift - 1, 4);
@@ -241,6 +242,29 @@ EncodeResult Encoder::encode_stream(std::istream& is, std::ostream& os, const En
                     fw.write_bit(0);
                 }
                 fw.write_bits(dmode_res.factor & 0x3FF, 10);
+            } else if (dmode_res.mode >= 6) {
+                if (dmode_res.shift > 0) {
+                    fw.write_bit(1);
+                    fw.write_bits(dmode_res.shift - 1, 4);
+                } else {
+                    fw.write_bit(0);
+                }
+                fw.write_bit(dmode_res.filter_order == 16 ? 1 : 0);
+                fw.write_bit(1); // dval1
+                fw.write_bit(0); // dval2
+                
+                for (int i = 0; i < dmode_res.filter_order; i += 4) {
+                    int max_val = 0;
+                    for (int j = 0; j < 4; j++) max_val = std::max(max_val, std::abs(dmode_res.filter[i + j]));
+                    int code_size = 0;
+                    while ((1 << code_size) <= max_val && code_size < 14) code_size++;
+                    if (code_size > 0) code_size++; // sign bit
+                    if (code_size < 7) code_size = 7;
+                    fw.write_bits(14 - code_size, 3);
+                    for (int j = 0; j < 4; j++) {
+                        fw.write_bits(dmode_res.filter[i + j] & ((1 << code_size) - 1), code_size);
+                    }
+                }
             }
         }
 
